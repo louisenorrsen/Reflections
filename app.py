7bfcf5cb41dbe3c0f41b23c969b741f3c98f4372
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-05-17 18:11:58 +0200

    Skapar notebook med flikar

--- app.py
+++ app.py
@@ -5,4 +5,20 @@ window = tk.Tk()
 window.title("Reflections")
 window.iconbitmap("diary.ico")
 
+# Notebook
+notebook = ttk.Notebook(window)
+notebook.pack(anchor="nw", expand=True, padx=10, pady=10)
+
+# Notebook Frames
+frm_today = ttk.Frame(notebook)
+frm_today.pack(fill="both", expand=True)
+frm_archive = ttk.Frame(notebook)
+frm_archive.pack(fill="both", expand=True)
+
+# Flikar
+style = ttk.Style()
+style.configure("TNotebook.Tab", padding=(10, 5))
+notebook.add(frm_today, text="Idag")
+notebook.add(frm_archive, text="Arkiv")
+
 tk.mainloop()
